25c50b620b6d2cb86145e6137035e14ddf3fa015
Victória Nascimento <victorianasc2@gmail.com>
2025-07-09 18:06:31 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,8 +4,8 @@ import "dotenv/config";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
-const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const NODE_ENV = process.env.NODE_ENV;
+const URL = "https://spotify-full-stack-ophe.onrender.com//api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
