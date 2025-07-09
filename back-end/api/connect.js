40e12ad20aa7ead1c056af9f66b830b87d8c02b7
Victória Nascimento <victorianasc2@gmail.com>
2025-07-09 14:48:45 -0300

    alterações de segurança banco de dados

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -2,11 +2,11 @@
 // await async
 // Fullfilled
 import { MongoClient } from "mongodb";
+import "dotenv/config"
 
-const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = process.env.MONGO_URI;
 
-const client = new MongoClient(URI);
+const client =  new MongoClient(process.env.MONGO_URI);
 
 export const db = client.db("spotifyAula");
 // const songCollection = await db.collection("songs").find({}).toArray();
