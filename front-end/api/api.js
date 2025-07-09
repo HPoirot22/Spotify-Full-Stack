e91d1d45caba31a62cdbee63ed966f6956c2da4a
Victória Nascimento <victorianasc2@gmail.com>
2025-07-09 18:24:57 -0300

    ajuste no dotenv/config

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,11 +1,10 @@
 // Fetch ou Axios
 import axios from "axios";
-import "dotenv/config";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
 // const NODE_ENV = process.env.NODE_ENV;
-const URL = "https://spotify-full-stack-ophe.onrender.com//api";
+const URL = "https://spotify-full-stack-ophe.onrender.com/api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
