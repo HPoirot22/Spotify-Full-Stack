a1a192e7af649b8a89058c414b9092b7e7e5554c
Victória Nascimento <163363216+HPoirot22@users.noreply.github.com>
2025-07-09 18:17:19 -0300

    Update api.js

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
 // const NODE_ENV = process.env.NODE_ENV;
-const URL = "https://spotify-full-stack-ophe.onrender.com//api";
+const URL = "https://spotify-full-stack-ophe.onrender.com/api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
